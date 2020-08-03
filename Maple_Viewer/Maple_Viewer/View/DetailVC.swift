662f763ec7e098d43ed095620df13add125ef30f
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-03 22:27:11 +0900

    [Modify] DetailViewController to DetailVC

--- Maple_Viewer/Maple_Viewer/View/DetailVC.swift
+++ Maple_Viewer/Maple_Viewer/View/DetailVC.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 
-class DetailViewController: UIViewController {
+class DetailVC: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
     
 }
