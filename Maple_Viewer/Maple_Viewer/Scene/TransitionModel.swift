148a420d54559838b6d8feb9f18c8434bc4d9f9c
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-03 23:11:03 +0900

    [Add] TransitionModel

--- Maple_Viewer/Maple_Viewer/Scene/TransitionModel.swift
+++ Maple_Viewer/Maple_Viewer/Scene/TransitionModel.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+enum TransitionStyle {
+    case root
+    case push
+    case modal
+}
+
+enum TransitionError: Error {
+    case navigationControllerMissing
+    case cannotPop
+    case unknown
+}
