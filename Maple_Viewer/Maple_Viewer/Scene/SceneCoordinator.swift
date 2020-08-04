e433fd3ce401f5b4fffea13ba84e2ac954ddd6f6
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-04 23:11:20 +0900

    [Delete] Scene group

--- Maple_Viewer/Maple_Viewer/Scene/SceneCoordinator.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SceneCoordinator.swift
-//  Maple_Viewer
-//
-//  Created by DohyunKim on 2020/08/03.
-//  Copyright © 2020 DohyunKim. All rights reserved.
-//
-
-import Foundation
--- Maple_Viewer/Maple_Viewer/Scene/SceneCoordinatorType.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SceneCoordinatorType.swift
-//  Maple_Viewer
-//
-//  Created by DohyunKim on 2020/08/03.
-//  Copyright © 2020 DohyunKim. All rights reserved.
-//
-
-import Foundation
